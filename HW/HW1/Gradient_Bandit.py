eebdf6d21fa92bebaf40c0a03ec9ed462e2a0a84
jtdorsey22 <jonathandorsey1117@gmail.com>
2021-04-07 02:11:44 -0700

    things

--- HW/HW1/Gradient_Bandit.py
+++ HW/HW1/Gradient_Bandit.py
@@ -1,86 +1,80 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import time
+from random import choices, choice
 
 def bandit_prob(action_taken, rw_mean_list):
+	
 	# Take Actual Mean from reward Mean, and use that to generate the normally distributed reward for the current action being taken
 	reward_mean = rw_mean_list[action_taken]
-	
 	reward = np.random.normal(reward_mean, 1)
 	
 	return reward
-	
-	
 
+def soft_max(H_pref, num_act):
+
+	pi = (np.exp(H_pref))/(np.sum(np.exp(H_pref)))
+
+	return pi 
+
+	
 if __name__ == '__main__':
 
 	t = time.time()
 	
-	k = 10 						  # Number of bandits
+	k = 10 			      # Number of bandits
 	num_runs = 1000 	  # Number of runs taken for a single bandit problem 
-	num_trial = 2000		# Number of bandit problems total  
-	# eps = .1 						# E-Greedy Parameter
+	num_trial = 2000	  # Number of bandit problems total  
 
-	eps_list = [0, .1, .01] 
+	alpha  = .1 		  # Step Size
+	pi = None
+	act_list = np.arange(k)	  # List of the action possible
 
+	reward_list = np.zeros((num_trial, num_runs))	# Initialize Reward List
+	avg_reward = np.zeros(num_runs)					# Initialize Average Reward List
+	opt_act = np.zeros((num_trial, num_runs))
+	opt_act_mean = np.zeros(num_runs)	
 
-	reward_list = np.zeros((num_trial, num_runs))
-	avg_reward = np.zeros((len(eps_list), num_runs))
-	
-	print(reward_list.shape)
+	for i in range(num_trial):		# Iterate over the total number of bandit trial
 
-	for ep_count, eps in enumerate(eps_list):
-	
-		for i in range(num_trial):		# Iterate over the total number of bandit trial
-			Q = np.zeros(k)			# Initialize Action Values to zeros 
-			N = np.zeros(k) 		 # Initialize "number of times bandit has been visited" to zeros
-		
-			actual_rewards = np.random.normal(0,1, k)	 # Use normal dist to set "actual" reward values for each bandit "trial"
-			
-			for j in range(num_runs):		# Iterate over the total number of runs for single bandit problem
+		if i % 100 == 0: 
+			print("Num Trial:", i)
+
+		H = np.zeros(k)
+		pi = soft_max(H, k)
+		Rt = 0
+
+		actual_rewards = np.random.normal(4, 1, k)	 # Use normal dist to set "actual" reward values for each bandit "trial"
+
+		for j in range(num_runs):		# Iterate over the total number of runs for single bandit problem
 				
-				eps_prob = np.random.uniform(0,1) 	# Compute epsilon probability for action selection 
-				
-				if j  ==0: 
-					action_num = np.random.randint(1,k)
-				
-				elif eps_prob > eps:
-					# if epsilon probability is greater than the epsilon parameter select action with max value 									
-					action_num = np.argmax(Q)
-					
+			action_num = choices(act_list, pi)	
+			R = bandit_prob(action_num, actual_rewards)
+			Rt = np.mean(reward_list[i,:])
+
+			if action_num == np.argmax(H): 
+				opt_act[i,j] = 1
+			else:
+				opt_act[i,j] = 0 
+
+
+			for a in range(k): 
+				if a == action_num: 
+					H[a] = H[a] + alpha*(R - Rt)*(1 - pi[a])
 				else:
-					# if epsilon probaility is less than the epsilon parameter select random action 
-					action_num = np.random.randint(1,k)
-				
-				R = bandit_prob(action_num, actual_rewards)
-				
-				reward_list[i,j] = R			
-								
-				N[action_num] += 1
-				Q[action_num] += (1./N[action_num])*(R - Q[action_num])
-				
-		for run in range(len(avg_reward[0,:])): 
-			avg_reward[ep_count, run] = np.mean(reward_list[:,run]) 
+					H[a] = H[a] - alpha*(R - Rt)*pi[a]
 
-	eps1 = avg_reward[0, :]
-	eps2 = avg_reward[1, :]
-	eps3 = avg_reward[2, :]
+			pi = soft_max(H, k)						# Action Probabilities
+			reward_list[i,j] = R	
 
-	# do stuff
-	elapsed = time.time()-t
+		for run in range(len(avg_reward[:])): 
+			avg_reward[run] = np.mean(reward_list[:,run]) 
+			opt_act_mean[run] = np.mean(opt_act[:,run])
+	# print(avg_reward)
 
-	print("Total Time Elapsed:", elapsed)
-
-	plt.plot(eps1)
-	plt.plot(eps2)
-	plt.plot(eps3)
-
-	plt.title("10-Armed Bandit")
-	plt.xlabel("Steps")
-	plt.ylabel("Average Reward")
-	plt.legend(["epsilon: 0","epsilon: 0.1","epsilon: 0.01"])
+	plt.plot(avg_reward)
+	plt.plot(opt_act_mean)
 	plt.show()
-
 
 
 	
