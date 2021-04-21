e8eb14ac208df0a58a0121b14e824fe4e7f20e0d
jtdorsey22 <jonathandorsey1117@gmail.com>
2021-04-21 00:26:09 -0700

    trying to get matlab version to work

--- HW/HW3/Dorsey_homework3.m
+++ HW/HW3/Dorsey_homework3.m
@@ -1,9 +1,43 @@
-%%
-
-% EEC 289A Homework #3 
+%% EEC 289A Homework #3 
 % Jonathan Dorsey 
 
+ %% 
+  
+ clear all 
+ clc
+            
 
+init_policy = ones(1,10);
+init_policy(1) = 0    ;      
+
+
+[optimal_policy, optimal_value_func] = policy_iteration(init_policy)
+
+
+%     opt_value_func, opt_policy = value_iteration()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%
 % Policy Iteration
 function V = policy_evaluation(policy)
 
@@ -16,10 +50,10 @@ function V = policy_evaluation(policy)
 
     while true                                    % Loop Forever, until desired precision is achieved  
 
-        delta = 0                                   % Initialize Error to zero at every loop start
+        delta = 0 ;                                  % Initialize Error to zero at every loop start
         counter = counter + 1 ;                               % Increment Counter 
 
-        for state  = [1:1: (max_num_states+1)]      % Loop Over all states in MDP 
+        for state  = [1:1: (max_num_states)]      % Loop Over all states in MDP 
 
             if (state-1) == 0                          % If state == 0, state does not transition 
                 P_h = 0 ;
@@ -66,7 +100,7 @@ function V = policy_evaluation(policy)
     
 end 
 
-function pi, value_func = policy_iteration(init_policy) 
+function [pi, value_func] = policy_iteration(init_policy) 
 
 
     value_func = policy_evaluation(init_policy) ;                            % Initial Value Func. Approximation (given deterministic Policy)
@@ -86,36 +120,40 @@ function pi, value_func = policy_iteration(init_policy)
                 imprv_counter
         end 
 
-        stable_flag, improved_policy = improvement_loop(pi, value_func);     % Use most recent Policy (pi) and Value Func. (value_func) and enter improvement function 
+        [stable_flag, improved_policy] = improvement_loop(pi, value_func);     % Use most recent Policy (pi) and Value Func. (value_func) and enter improvement function 
         pi = improved_policy;                                                % Once new policy is output, update the old policy to the new policy 
-        value_func = policy_evaluation(pi)    ;                              % Once new value func. is output, update the old value func. to the new value func. 
+        value_func = policy_evaluation(pi);                              % Once new value func. is output, update the old value func. to the new value func. 
     end                                                                     % Repeat UNTIL policy has converged and STABLE_FLAG is set to TRUE by "impprovement_loop"
 %     print(f"Policy Improvement Counter: {imprv_counter}")                   % Once Policy is Converged Print Loop Counter 
 
 end
 
         
-function stability, new_policy  = improvement_loop(pi, V_func) 
+function [stability, new_policy]  = improvement_loop(pi, V_func) 
 
 
     gamma = 1 ;                                                                  % Discount Factor 
     max_state_num = 10  ;                                                        % Max Number of states in MDP 
 
-    old_action = [pi[state] for state in range(0, max_state_num)] ;              % Create copy of old policy via list comprehension 
+    
+    for i  = [1:1:(max_state_num)]
+        old_action(i) = pi(i);
+    end
+    
     new_policy = zeros(1, max_state_num);                                       % Create array for new policy (initialized to zeros)
 
     stability = true      ;                                                      % Set Stability -> True to test for improved policy convergence 
 
-    for state  = [1:1:(max_state_num + 1)]                                       % Loop over every state in the MDP 
+    for state  = [1:1:(max_state_num)]                                       % Loop over every state in the MDP 
       
-        if state == 0:                                                          % If the current state == 0 the only action possible is the zero action ($0 bet)
-            new_policy(state) = 0                                               % Update zero-th element of new policy with only action possible from state == 0 
+        if (state-1) == 0                                                          % If the current state == 0 the only action possible is the zero action ($0 bet)
+            new_policy(state) = 0 ;                                              % Update zero-th element of new policy with only action possible from state == 0 
             continue  
         end % Skip remaining loop and continue to next state in MDP 
 
-        action_values = (-inf) ;                                              % Initialize Action-Value function for CURRENT state NOTE: Q(0) = -infinity to garuntee argmax improvement does no select infeasible action   
+        action_values = [-inf] ;                                                 % Initialize Action-Value function for CURRENT state NOTE: Q(0) = -infinity to garuntee argmax improvement does no select infeasible action   
 
-        for bet  = [1:1:(state+1)]                                          % For every possible action (aka Bet) possible from the current state: Compute the 
+        for bet  = [2:1:state]                                                   % For every possible action (aka Bet) possible from the current state: Compute the 
             Q = 0;                                                               % Action value for current action TAKEN from the current state
             P_h = .9 ;                                                           % functionine Probability of Transition into heads ONCE action (aka bet) is taken                                                          
             P_t = .1 ;                                                           % functionine Probability of Transition into tails ONCE action (aka bet) is taken 
@@ -140,17 +178,19 @@ function stability, new_policy  = improvement_loop(pi, V_func)
             action_values(end+1) = (Q)   ;                                          % Append Q to list of action values for the CURRENT state 
         end 
             
-        max_val, arg_max = max(action_values)
-        new_policy(state) = arg_max                                             % Once all possible actions have been taken (for current state) select the MAX Action-Value 
+        [max_val, arg_max] = max(action_values);
+        new_policy(state) = arg_max  ;                                           % Once all possible actions have been taken (for current state) select the MAX Action-Value 
 
         if old_action(state) ~= new_policy(state)                               % Check for Convergence Stabilty. IF the old Policy does not match the new Policy then the policy has not finished converging 
-            stability = False 
+            stability = false ;
         end 
                                                                                 % Return: The stability flag and the updated (improved) policy
     end
+    
+end
 
 % Value Iteration 
-function V_func, new_policy = value_iteration() 
+function [V_func, new_policy] = value_iteration() 
 
     max_num_states = 10;                             % Max Number of States in MDP
     V = zeros(1, max_num_states);                    % Initialize Value Func. to zeros
@@ -182,7 +222,7 @@ function V_func, new_policy = value_iteration()
 
             v_state = V(state);                      % Store copy of initial value (at current state) for comparison later
 
-            max_state_value = (-inf)                                               % Initialize Action-Value function for CURRENT state NOTE: Q(0) = -infinity to garuntee argmax improvement does no select infeasible action   
+            max_state_value = [-inf];                                               % Initialize Action-Value function for CURRENT state NOTE: Q(0) = -infinity to garuntee argmax improvement does no select infeasible action   
 
             for bet = [1:1:(state+1)]                                         % For every possible action (aka Bet) possible from the current state: Compute the 
                 v = 0  ;                                                             % Action value for current action TAKEN from the current state                                                         % functionine Probability of Transition into tails ONCE action (aka bet) is taken 
@@ -207,8 +247,8 @@ function V_func, new_policy = value_iteration()
                 max_state_value(end+1) = v ;                                            % Append Q to list of action values for the CURRENT state 
             end
             
-            V(state) = max(max_state_value)
-            delta = max(delta, abs(v_state - V(state)))  % Compute delta (precision error)
+            V(state) = max(max_state_value);
+            delta = max(delta, abs(v_state - V(state))) ; % Compute delta (precision error)
         end 
         
         if delta < theta                           % if precision error is less than precision parameter STOP policy evaluation 
@@ -216,6 +256,8 @@ function V_func, new_policy = value_iteration()
         end 
         
     end
+    
+
 
 
 
@@ -265,45 +307,10 @@ function V_func, new_policy = value_iteration()
             max_val, argmax = max(action_values)  ;  
             new_policy(state) = argmax;                            % Once all possible actions have been taken (for current state) select the MAX Action-Value 
     end 
-                                        
+end                                
             
             
             
+     
             
             
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-
-    init_policy = randi(1, 9,[1,10])
-    init_policy(0) = 0                                                         
-
-
-
-    print("Initial Random Policy ",init_policy)
-
-    optimal_policy, optimal_value_func = policy_iteration(init_policy)        % Perform Policy Improvement and output: Optimal Policy & Optimal Value Function
-
-    print("Optimal Policy")
-    print(optimal_policy)
-    print("Optimal Value Function")
-    print(optimal_value_func)
-
-
-    opt_value_func, opt_policy = value_iteration()
-    print("Optimal Policy")
-    print(opt_policy)
-    print("Optimal Value Function")
-    print(opt_value_func)
