0e92d49d76af59f1426a275166a75f64234e6dde
jtdorsey22 <jonathandorsey1117@gmail.com>
2021-04-25 00:14:11 -0700

    working on Black trajectory generation

--- Algorithms/Chapter5_MonteCarloMethods/hw4.py
+++ Algorithms/Chapter5_MonteCarloMethods/hw4.py
@@ -41,7 +41,7 @@ card_values = {1: (1,11), 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10
 
 policy_dict = ["hit": 0, "stick": 1]
 
-def dealer_action(dealer_faceup_card, deck_of_cards, card_cnt, dealer_policy==1):
+def dealer_action(state, deck_of_cards, card_cnt, dealer_policy=1):
     """
     Dealer Must follow fixed policy about the game. 
 
@@ -52,23 +52,47 @@ def dealer_action(dealer_faceup_card, deck_of_cards, card_cnt, dealer_policy==1)
     Case #1: Dealer HITS on 17 
     Case #2: Dealer STICKS on 17 
     """
+    done = False                                    # Initialize DONE flag to False
+    dealer_value = state[1]                         # Initialize new state of the dealer to the current state (will be updated depending on action taken)
 
-    new_dealer_state = dealer_faceup_card               # Initialize new state of the dealer to the current state (will be updated depending on action taken)
+    while done is not True:                         # While DEALER_VAL <17
 
-    if dealer_policy == 1:                              # HIT on 17 
-        
-        new_dealer_state += deck_of_cards[card_cnt]
-        card_cnt += 1
+        dealer_value += deck_of_cards[card_cnt]     # Dealer Draws a new card and Updates dealer_value
+        card_cnt += 1                               # Update the card index 
 
-        return new_dealer_state, card_cnt
+        if dealer_policy == 1:                      # HIT on 17 
+            if dealer_value >17:                    # If dealer_value is >17 set DONE -> True
+                done = True                         
 
-    elif dealer_policy ==2:                             # STICK on 17 
+        if dealer_policy == 2:                      # STICK on 17 
+            if dealer_value =>17:                   # If Dealer_value is => 17 set DONE -> True
+                done = True
 
-        return new_dealer_state, card_cnt
+    return dealer_value, card_cnt
+
+
+def policy(player_state, dealer_state, useable_ace): 
+
+
+
+
+    return 
+
+def check_ace_useability(current_state, new_card_value): 
+
+    current_value = current_state[0]                # Get Current Player SUM value from the current STATE               
+
+    if new_card_value == 1:                         # Check if new card value is an ACE (default value = 1)
+
+        useable_ace_value = current_value + 11      # IF new card is an ACE, check whether a value of 11 is still value
+
+        if useable_ace_value <= 21:                  # If old player value + 11 is valid (aka <=21) then return True
+            return True
+        else: 
+            return False                            # If the ace is NOT useable return False
 
     else: 
-
-        # DO NOTHING
+        return False                                # If new card is NOT an ACE return False
 
 def create_deck(): 
 
@@ -96,72 +120,53 @@ def generate_blackjack_episode(policy, dealer_policy):
     episode_trajectory = [] 
 
     # Initialize States Randomly (Exploring Starts)
-    init_state = (random.randint(0,21), random.randint(1,10), random.randint(0,1))
+    state = (random.randint(0,21), random.randint(1,10), random.randint(0,1))
 
-    state = init_state
+    done = False                                    # Set DONE Flag for End of Episode 
+    card_counter = 0                                # Initialize Counter for which current card to pull from deck                            
 
-    done = False                                # Set DONE Flag for End of Episode 
+    R_intermediate = 0                              # Intermediate Rewards during Game are ZERO
+    new_state = state                               # Initialize the New state from the current state
 
-    while done is not True:                     # While EPISODE is not DONE Loop until terminal state is achieved
-        
-        card_counter = 0 
+    while done is not True:                         # While Player is still HITTING loop
 
+        player_action = policy(state)               # Sample Players action from the current state of the MDP
 
-        player_state = state[0]
-        dealer_state = state[1]
-        player_useable_ace = state[2]
+        if player_action == 0:                      # Player HITS
 
+            new_state[0] += card_deck[card_counter] # Player Dealt single card (Update Value of Players hand)
+            ace_check = check_ace_useability(state, card_deck[card_counter])    # Check if new Card is a useable ACE    
+            card_counter += 1                        # Increment the deck counter
 
-        player_action = policy[state]
+            if ace_check is True:                   # If ACE is useable update new state 
+                new_state[2] = True                 # Update state[2] tuple value
 
-        
-        if player_action == 0:                                      # Player HITS
+            else: 
+                # New Card is either NOT an ace 
+                # OR 
+                # Not a useable ACE
+                
+        elif player_action == 1:                    # Player STICKS
+            done = True                             # Change Loop Stopping Condition 
 
-            player_state += card_deck[card_counter]
-            card_counter +=1 
+        episode_trajectory.append(state, player_action, R_intermediate, new_state)
+        state = new_state                           # Update the current state to the new state
 
-            dealer_state, card_counter = dealer_action(dealer_state, card_deck, card_counter, dealer_policy)
+    dealer_val, card_counter = dealer_action(state, card_deck, card_counter, dealer_policy)
 
-        elif player_action == 1:                                    # Player STICKS
+    if state[0] > 21 or state[0] < dealer_val: 
 
-            dealer_state, card_counter = dealer_action(dealer_state, card_deck, card_counter, dealer_policy)
+        R_game = -1
 
+    if state[0] ==21 and dealer_val != 21: 
 
+        R_game = 1
 
+    if state[0] ==dealer_val: 
 
-        if player_state == 21 and dealer_state == 21:               # DRAW 
+        R_game = 0 
 
-        elif player_state == 21 and dealer_state != 21:             # WIN
-
-        elif player_state > 21                                      # BUST
-
-        elif player_state == dealer_state:                          # DRAW
-
-        elif player_state > dealer_state:                           # WIN 
-
-        else: 
-
-
-
-
-
-
-
-
-
-
-
-
-        else:
-            # DO NOTHING Dealer Policy is Undefined
-
-
-
-
-    
-
-
-
+    episode_trajectory.append(state, 1, R_game, state)
 
 
 
